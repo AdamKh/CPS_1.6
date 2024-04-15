c9bea7594280ab8205ced3f011be274bc5c828ee
Adam Khashiev <adamkhashiev2004@mail.ru>
2024-04-15 23:31:37 +0300

    added swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,79 @@
+import Swiper from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
+import '../../node_modules/swiper/swiper.scss'
 import '../scss/style.scss'
 
-import slider from './slider.js'
-slider()
+let screenWidth = document.documentElement.clientWidth
+const swiperWrapper = document.querySelector('.swiper-wrapper')
+let mobileDevice = false,
+  tableDevice = false,
+  pcDevice = false
 
-import show_more from './show_more.js'
-show_more()
+if (screenWidth >= 768 && screenWidth < 1120) {
+  tableDevice = true
+} else if (screenWidth >= 1120) {
+  pcDevice = true
+} else {
+  mobileDevice = true
+}
+
+if (mobileDevice) {
+  var swiper = new Swiper('.repair-brands__catalog--mySwiper', {
+    modules: [Navigation, Pagination],
+    slidesPerView: 'auto',
+    spaceBetween: 0,
+    freeMode: true,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  })
+} else {
+  swiperWrapper.classList.add('repair-brands__catalog--grid')
+}
+
+const showMoreBrands = document.querySelector('.repair-brands__show-more')
+const showMoreBrandsIcon = document.querySelector(
+  '.repair-brands__show-more .show-more__icon'
+)
+
+const brandCardTableHidden = document.querySelectorAll(
+  '.brand-card--table-hidden'
+)
+const brandCardPcHidden = document.querySelectorAll('.brand-card--pc-hidden')
+
+const btn = document.querySelector('.repair-brands__show-more')
+let btnClassList = btn.classList
+
+showMoreBrands.addEventListener('click', function () {
+  if (Array.from(btnClassList).includes('repair-brands__show-more--show')) {
+    btn.textContent = 'Скрыть'
+
+    for (let i = 0; i < brandCardTableHidden.length; i++) {
+      brandCardTableHidden[i].classList.remove('brand-card--table-hidden')
+      brandCardTableHidden[i].classList.remove('brand-card--pc-hidden')
+      brandCardTableHidden[i].classList.add('brand-card--table-show')
+    }
+
+    btn.classList.remove('repair-brands__show-more--show')
+    btn.classList.add('repair-brands__show-more--close')
+  } else {
+    btn.textContent = 'Показать все'
+
+    if (pcDevice) {
+      for (let i = 0; i < brandCardPcHidden.length; i++) {
+        brandCardPcHidden[i].classList.remove('brand-card--pc-show')
+        brandCardPcHidden[i].classList.add('brand-card--pc-hidden')
+      }
+    } else {
+      for (let i = 0; i < brandCardTableHidden.length; i++) {
+        brandCardTableHidden[i].classList.remove('brand-card--table-show')
+        brandCardTableHidden[i].classList.add('brand-card--table-hidden')
+      }
+    }
+
+    btn.classList.remove('repair-brands__show-more--close')
+    btn.classList.add('repair-brands__show-more--show')
+  }
+})
